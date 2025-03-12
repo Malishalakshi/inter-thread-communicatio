533e63e8d9e4797051a228ac201218cf5e3e4c98
Maleesha Lakshani <malishalakshaniyapa@gmail.com>
2025-03-12 13:36:57 +0530

    Stuctured as last project

--- src/main/java/lk/ijse/dep13/interthreadcomminicationexample/ObtainConnectionServlet.java
+++ src/main/java/lk/ijse/dep13/interthreadcomminicationexample/ObtainConnectionServlet.java
@@ -1,15 +1,26 @@
 package lk.ijse.dep13.interthreadcomminicationexample;
 
 import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import lk.ijse.dep13.interthreadcomminicationexample.db.MontisoriCP;
 
 import java.io.IOException;
+import java.io.PrintWriter;
 
+@WebServlet(name = "obtain-connection-servlet" ,urlPatterns = "/connection/random")
 public class ObtainConnectionServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        System.out.println("ObtainConnectionServlet");
+        MontisoriCP connectionPool = (MontisoriCP) getServletContext().getAttribute("datasource");
+        MontisoriCP.ConnectionWrapper cWrapper = connectionPool.getConnection();
+
+        resp.setContentType("text/html");
+        PrintWriter out = resp.getWriter();
+        out.printf("<h1>Id: %s</h1>", cWrapper.id());
+        out.printf("<h1>Connection Ref: %s</h1>", cWrapper.connection());
     }
 }
+
--- src/main/java/lk/ijse/dep13/interthreadcomminicationexample/db/MontisoriCP.java
+++ src/main/java/lk/ijse/dep13/interthreadcomminicationexample/db/MontisoriCP.java
@@ -1,4 +1,84 @@
 package lk.ijse.dep13.interthreadcomminicationexample.db;
 
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.HashMap;
+import java.util.Properties;
+
 public class MontisoriCP {
+    private static final int DEFAULT_POOL_SIZE = 4;
+
+    private final HashMap<Integer, Connection> MAIN_POOL = new HashMap<>();
+    private final HashMap<Integer, Connection> CONSUMER_POOL = new HashMap<>();
+    private final int poolSize;
+
+    public MontisoriCP() {
+        this(DEFAULT_POOL_SIZE);
+    }
+
+    public MontisoriCP(int poolSize) {
+        this.poolSize = poolSize;
+        try {
+            initializePool();
+        } catch (IOException | SQLException | ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public int getPoolSize() {
+        return poolSize;
+    }
+
+    private void initializePool() throws IOException, SQLException, ClassNotFoundException {
+        Properties properties = new Properties();
+        properties.load(getClass().getResourceAsStream("/application.properties"));
+
+        String host = properties.getProperty("app.db.host");
+        String port = properties.getProperty("app.db.port");
+        String database = properties.getProperty("app.db.database");
+        String user = properties.getProperty("app.db.user");
+        String password = properties.getProperty("app.db.password");
+
+        Class.forName("com.mysql.cj.jdbc.Driver");
+
+        for (int i = 0; i < poolSize; i++) {
+            Connection connection = DriverManager.getConnection("jdbc:mysql://%s:%s/%s"
+                    .formatted(host, port, database), user, password);
+            MAIN_POOL.put((i + 1) * 10, connection);
+        }
+    }
+
+    public synchronized ConnectionWrapper getConnection() {
+        while (MAIN_POOL.isEmpty()) {
+            try {
+                wait();
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }
+        Integer key = MAIN_POOL.keySet().stream().findFirst().get();
+        Connection connection = MAIN_POOL.get(key);
+        MAIN_POOL.remove(key);
+        CONSUMER_POOL.put(key, connection);
+        return new ConnectionWrapper(key, connection);
+    }
+
+    public synchronized void releaseConnection(Integer id){
+        if (!CONSUMER_POOL.containsKey(id)) throw new RuntimeException("Invalid Connection ID");
+        Connection connection = CONSUMER_POOL.get(id);
+        CONSUMER_POOL.remove(id);
+        MAIN_POOL.put(id, connection);
+        notify();
+    }
+
+    public synchronized void releaseAllConnections(){
+        CONSUMER_POOL.forEach(MAIN_POOL::put);
+        CONSUMER_POOL.clear();
+        notifyAll();
+    }
+
+    public record ConnectionWrapper(Integer id, Connection connection) {
+    }
 }
